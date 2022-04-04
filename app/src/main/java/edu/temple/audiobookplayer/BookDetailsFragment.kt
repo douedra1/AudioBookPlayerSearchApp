07581d9d4ff70f0b2402c21747e6a5df15d43531
djakaridjaouedraogo <tup09204@temple.edu>
2022-04-03 20:18:13 -0400

    commit

---
+++ app/src/main/java/edu/temple/audiobookplayer/BookDetailsFragment.kt
@@ -0,0 +1,73 @@
+package edu.temple.audiobookplayer
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import edu.temple.audiobookplayer.R
+
+import com.squareup.picasso.Picasso
+
+class BookDetailsFragment : Fragment() {
+
+    private lateinit var tvClickedBookTitle: TextView
+    private lateinit var tvClickedBookAuthor: TextView
+    private lateinit var ivBookCover: ImageView
+    private lateinit var book: Book;
+    private val viewModel: AppViewModel by activityViewModels()
+
+
+    companion object {
+
+        fun newInstance(book: Book): BookDetailsFragment {
+            val fragmentDetails = BookDetailsFragment();
+            fragmentDetails.book = book
+            return fragmentDetails;
+        }
+        fun newInstance(): BookDetailsFragment {
+            val fragmentDetails = BookDetailsFragment();
+            return fragmentDetails;
+        }
+    }
+
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view =  inflater.inflate(R.layout.frag_book_details, container, false)
+        findView(view)
+        return view;
+    }
+
+    private fun findView(view: View){
+        tvClickedBookTitle = view.findViewById(R.id.tvClickedBookTitle)
+        tvClickedBookAuthor = view.findViewById(R.id.tvClickedBookAuthor)
+        ivBookCover = view.findViewById(R.id.ivBookCover)
+        initData();
+    }
+
+    private fun initData(){
+
+        viewModel.selectedBook.observe(viewLifecycleOwner) { book ->
+            setBookDate(book)
+        }
+
+        if(::book.isInitialized){
+            setBookDate(book)
+        }
+
+    }
+
+    private fun setBookDate(book: Book){
+        tvClickedBookTitle.text = book.title
+        tvClickedBookAuthor.text = book.author
+        var bookUrl:String = book.cover_url.replace("\\/","");
+        Picasso.get().load(bookUrl).into(ivBookCover);
+    }
+
+
+}
---
+++ app/src/main/java/edu/temple/audiobookplayer/MainActivity.kt
@@ -0,0 +1,182 @@
+package edu.temple.audiobookplayer
+
+
+import android.app.SearchManager
+import android.content.Intent
+import android.content.res.Configuration
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import edu.temple.audiobookplayer.R
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var fm: FragmentManager;
+    private lateinit var btnSearch: Button;
+    private lateinit var progressBar: ProgressBar;
+    private lateinit var bookList: BookList;
+    private val viewModel: AppViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initDate();
+        handleIntent(intent)
+    }
+
+
+    private fun initDate(){
+        btnSearch = findViewById(R.id.btnSearch)
+        progressBar = findViewById(R.id.progress_bar)
+        bookList =  BookList();
+        clickListener()
+
+        fm = supportFragmentManager
+
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_PORTRAIT){
+            displayFragments(1);
+        }
+        else {
+            displayFragments(2);
+        }
+        handleViewModel();
+    }
+
+    private fun handleViewModel(){
+        val orientation = resources.configuration.orientation
+
+        viewModel.selectedBook.observe(this) { book ->
+            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                loadBookListFragment(BookDetailsFragment.newInstance(book));
+            }
+        }
+
+        viewModel.searchBookMessage.observe(this){
+            if(it != null){
+                handleProgressBar(false)
+                showMessage(it)
+            }
+        }
+
+        viewModel.booksResponse.observe(this){
+            handleProgressBar(false)
+            setBookCollections(it)
+        }
+    }
+
+    private fun handleProgressBar(show: Boolean){
+        if(show){
+            progressBar.visibility = View.VISIBLE
+        }else{
+            progressBar.visibility = View.GONE
+
+        }
+
+    }
+
+    private fun clickListener(){
+
+        btnSearch.setOnClickListener {
+            onSearchRequested();
+        }
+
+    }
+
+    private fun displayFragments(type: Int){
+
+        if (type == 1) {
+            loadBookListFragment(BookListFragment.newInstance(bookList.getBookArray()))
+        } else {
+            loadBookListFragment(BookListFragment.newInstance(bookList.getBookArray()));
+            loadBookDetailFragment();
+        }
+    }
+
+    private fun loadBookListFragment(fragment: Fragment) {
+        // load fragment
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.book_lists, fragment,"list")
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+    }
+
+    private fun loadBookDetailFragment() {
+        // load fragment
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.book_details, BookDetailsFragment.newInstance(),"detail")
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+    }
+
+
+    private fun setBookCollections(books: List<Book>){
+
+
+        var list =  books;
+        bookList.setBookArray(ArrayList(list))
+        displayFragments(1);
+    }
+
+    class BookList{
+
+        private  var bookArray: ArrayList<Book> = arrayListOf();
+
+        fun setBookArray(bookArray: ArrayList<Book>){
+            this.bookArray = bookArray
+        }
+
+        fun getBookArray():ArrayList<Book>{
+            return bookArray;
+        }
+
+        fun addBook(book: Book){
+            this.bookArray.toMutableList().add(book)
+        }
+
+        fun removeBook(book: Book){
+            this.bookArray.toMutableList().remove(book)
+        }
+
+        fun getBook(position: Int): Book {
+            return this.bookArray.toMutableList()[position]
+        }
+
+
+        fun getBookSize():Int{
+            return this.bookArray.toMutableList().size
+        }
+
+
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+                handleProgressBar(true)
+                viewModel.searchBook(query)
+            }
+        }
+    }
+
+    private fun showMessage(message: String){
+        Toast.makeText(this@MainActivity,message,Toast.LENGTH_LONG).show()
+    }
+
+}
